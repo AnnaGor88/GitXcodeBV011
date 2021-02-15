f96fda2725d891528c82decb5eddfa32ab8c1519
Anna <goryachevaanny@gmail.com>
2021-02-15 20:31:43 +0300

    My first commit

--- GitXcodeBV011/main.cpp
+++ GitXcodeBV011/main.cpp
@@ -6,9 +6,49 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void FillRand(int* arr, const int n);
+void Print(int arr[], const int n);
+
+
+int main()
+{
+    setlocale (LC_ALL, "");
+    int n;
+    cout << " Введите размер массива: "; cin >> n;
+    int* arr = new int[n] {};
+    cout << arr << endl;
+    FillRand(arr,n);
+    Print(arr,n);
+    
+//    int brr[5];
+//    cout << brr << endl;
+//    FillRand(brr,5);
+//    Print(brr,5);
+    
+    int value; // добавляемые значения
+    cout << "Введите добавляемое значение: "; cin >> value;
+    arr[n]=value;
+    n++;
+    Print(arr,n);
+    int* buffer[n+1];
+    delete[]arr;
+    
     return 0;
+}
+void FillRand(int arr[], const int n)
+{
+for (int i=0; i<n; i++)
+{
+    *(arr+i)=rand()%100;    // * - возвращает значение по адресу
+}
+}
+void Print(int arr[], const int n)
+{
+for (int i=0; i<n; i++)
+{
+    cout << arr[i] << "\t";   // оператор индексирования [] возвращает значение по индексу
+}
+cout << endl;
 }
