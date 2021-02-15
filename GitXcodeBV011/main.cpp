d0650c8ffc543880566b13225d16fb1ff0bab890
Anna <goryachevaanny@gmail.com>
2021-02-15 19:35:37 +0300

    Initial Commit

---
+++ GitXcodeBV011/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitXcodeBV011
+//
+//  Created by Анна Горячева on 15.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
